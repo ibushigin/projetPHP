8fff207493aa0828bc4544581aaf870428b4de00
ibushigin <rq6v6k3>
2018-11-30 14:40:35 +0100

    Update add-user.php

--- add-user.php
+++ add-user.php
@@ -43,13 +43,15 @@ require_once('inc/connexion.php');
             </select>
           </div>
 
-          <button class="btn btn-dark" type="submit" name="button">Envoyer</button>
+          <button class="btn btn-dark" type="submit" name="btnAdd">Envoyer</button>
         </form>
       </div>
 <!-- PHP POUR L'AJOUT DE L'USER -->
 <?php
 //Verif du POST
 if(!empty($_POST)){
+  if(isset($_POST['btnAdd'])){
+
   $errors = [];
   $post = [];
   foreach($_POST as $key => $value){
@@ -102,6 +104,7 @@ if(!empty($_POST)){
     echo implode('<br>', $errors);
   }
 }
+}
  ?>
     <div class="col-md-4">
       <h2>Modifier un utilisateur</h2>
@@ -113,7 +116,7 @@ if(!empty($_POST)){
             <div class="form-group">
                 <label>Nom de l'utilisateur</label>
                 <select name="idUser" class="form-control">
-                    <option value="0">Choisir un user</option>
+                    <option value="0">Choisir un utilisateur</option>
                     <?php
                     foreach($users as $user){
                         ?>
@@ -125,42 +128,176 @@ if(!empty($_POST)){
             </div>
             <button class="btn btn-dark">Choisir</button>
         </form>
-  <?php
+        <?php
+        if(!empty($_GET['idUser']) && preg_match("#^\d+$#", $_GET['idUser'])){
+            //récupération des infos de l'utilisateur
+            $select = $connexion->prepare('SELECT * FROM users WHERE id = :id');
+            $select->bindValue(':id', $_GET['idUser']);
+            $select->execute();
+            $user = $select->fetch();
+        ?>
+        <hr>
+                <form method="post" class="mt-5">
+                  <div class="form-group">
+                      <label>Nom</label>
+                      <input type="text" name="name" class="form-control" value="<?= $user['name'] ?>">
+                  </div>
+                  <div class="form-group">
+                      <label>Email</label>
+                      <input type="text" name="email" class="form-control" value="<?= $user['email'] ?>">
+                  </div>
+                  <div class="form-group">
+                      <label>Role</label>
+                      <select name="role" class="form-control">
+                          <option value="ROLE_VENDOR" <?= $user['role'] === "ROLE_VENDOR" ? 'selected' : ''; ?> >Vendeur</option>
+                          <option value="ROLE_ADMIN" <?= $user['role'] === "ROLE_ADMIN" ? 'selected' : ''; ?> >Admin</option>
+                      </select>
+                  </div>
+                  <input type="hidden" name="idUser" value="<?= $_GET['idUser'] ?>">
+                  <button type="submit" class="btn btn-dark" name="btnModif">Modifier information</button>
+                </form>
 
+        <?php
+        }
+        ?>
+<!-- MODIFICATION MDP -->
+        <h2>Modifier le mot de passe</h2>
+        <?php
+        $select = $connexion->query('SELECT id, name FROM users');
+        $users = $select->fetchAll();
+        ?>
+          <form class="mb-5">
+              <div class="form-group">
+                  <label>Nom de l'utilisateur</label>
+                  <select name="idPwd" class="form-control">
+                      <option value="0">Choisir un utilisateur</option>
+                      <?php
+                      foreach($users as $user){
+                          ?>
+                          <option value="<?= $user['id'] ?>"><?= $user['name'] ?></option>
+                          <?php
+                      }
+                      ?>
+                  </select>
+              </div>
+              <button class="btn btn-dark">Choisir</button>
+          </form>
+          <?php
+          if(!empty($_GET['idPwd']) && preg_match("#^\d+$#", $_GET['idPwd'])){
+              //récupération des infos de l'utilisateur
+              $select = $connexion->prepare('SELECT * FROM users WHERE id = :id');
+              $select->bindValue(':id', $_GET['idPwd']);
+              $select->execute();
+              $user = $select->fetch();
+          ?>
+          <hr>
+                  <form method="post" class="mt-5">
+                    <div class="form-group">
+                      <label for="password">Entrez le nouveau mot de passe</label>
+                      <input type="password" name="password3" class="form-control">
+                    </div>
 
-if(!empty($_GET['idUser']) && preg_match("#^\d+$#", $_GET['idUser'])){
-    //récupération des infos de l'utilisateur
-    $select = $connexion->prepare('SELECT * FROM users WHERE id = :id');
-    $select->bindValue(':id', $_GET['idUser']);
-    $select->execute();
-    $user = $select->fetch();
-?>
-<hr>
-        <form method="post" class="mt-5">
-          <div class="form-group">
-              <label>Nom</label>
-              <input type="text" name="Nom" class="form-control" value="<?= $user['name'] ?>">
-          </div>
-          <div class="form-group">
-              <label>Email</label>
-              <input type="text" name="email" class="form-control" value="<?= $user['email'] ?>">
-          </div>
-          <div class="form-group">
-              <label>Role</label>
-              <select name="role" class="form-control">
-                  <option value="0">Choisir un role</option>
-                  <option value="ROLE_AUTEUR" <?= $user['role'] === "ROLE_VENDOR" ? 'selected' : ''; ?> >Vendeur</option>
-                  <option value="ROLE_ADMIN" <?= $user['role'] === "ROLE_ADMIN" ? 'selected' : ''; ?> >admin</option>
-              </select>
-          </div>
-          <input type="hidden" name="idUser" value="<?= $_GET['idUser'] ?>">
-          <button type="submit" class="btn btn-dark">Ajouter</button>
-        </form>
+                    <div class="form-group">
+                      <label for="password2">Répétez le nouveau mot de passe</label>
+                      <input type="password" name="password4" class="form-control">
+                    </div>
+                    <input type="hidden" name="idPwd" value="<?= $_GET['idPwd'] ?>">
+                    <button type="submit" class="btn btn-dark" name="btnPwd">Modifier information</button>
+                  </form>
+
       </div>
     </div>
   </div>
 <?php
+
 }
+//TRAITEMENT Modification user
+if(!empty($_POST)){
+    if(isset($_POST['btnModif'])){
+    $post = [];
+
+    foreach($_POST as $key => $value){
+        $post[$key] = strip_tags(trim($value));
+    }
+
+    $errors = [];
+
+    if(empty($post['name']) || mb_strlen($post['name']) < 4 || mb_strlen($post['name']) > 25){
+      $errors[] = 'Le nom doit faire entre 4 et 25 caractèressssss';
+    }
+  //Verif du role
+    if(!isset($post['role']) || (($post['role']) != 'ROLE_ADMIN' && ($post['role']) != 'ROLE_VENDOR')){
+        $errors['role'] = 'rôle pas trouvé';
+      }
+  //Verif de l'email
+    if(empty($post['email']) OR !filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
+          $errors['email'] = 'email invalide';
+    }
+    $select = $connexion->prepare('SELECT id FROM users WHERE email = :email AND id <> :id');
+    $select->bindValue(':email', $post['email']);
+    $select->bindValue(':id', $post['idUser']);
+    $select->execute();
+    $users = $select->fetchAll();
+    if(count($users) > 0){
+        $errors[] = 'email déjà présent';
+    }
+    if(empty($errors)){
+
+        $update = $connexion->prepare('UPDATE users SET name = :nom, email  = :email, role = :role WHERE id = :id');
+        $update->bindValue(':nom', $post['name']);
+        $update->bindValue(':email', $post['email']);
+        $update->bindValue(':role', $post['role']);
+        $update->bindValue(':id', $post['idUser']);
+        if($update->execute()){
+            echo 'Utilisateur modifié';
+        }
+
+    }
+    else{
+        echo implode('<br>', $errors);
+    }
+}
+}
+
+// TRAITEMENT MODIFICATION PASSWORD
+if(!empty($_POST)){
+    if(isset($_POST['btnPwd'])){
+
+    $post = [];
+
+    foreach($_POST as $key => $value){
+        $post[$key] = strip_tags(trim($value));
+    }
+
+    $errors = [];
+
+    if(!isset($post['password3']) OR !isset($post['password4'])
+      OR mb_strlen($post['password3']) < 4
+      OR mb_strlen($post['password3']) > 10){
+        $errors['mdp'] = 'le mot de passe doit faire entre 4 et 10 caractères';
+    }
+    if($post['password3'] !== $post['password4']){
+          $errors['mdp'] = 'Les deux mdp envoyés doivent être identiques';
+      }
+
+
+    if(empty($errors)){
+        $update = $connexion->prepare('UPDATE users SET password = :password WHERE id = :id');
+        $update->bindValue(':password', password_hash($post['password3'], PASSWORD_DEFAULT));
+        $update->bindValue(':id', $post['idPwd']);
+        if($update->execute()){
+            echo 'Mot de passe modifié';
+        }
+
+    }
+    else{
+        echo implode('<br>', $errors);
+    }
+}else{
+  echo 'bordel';
+}
+}
+
 ?>
 
 
