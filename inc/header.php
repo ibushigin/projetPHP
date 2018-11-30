2aa5e57f26112c3625f74fe6c3eeb4d4eb2d95e7
Darpen <ben.peyrat@gmail.com>
2018-11-30 15:22:21 +0100

    header css

--- inc/header.php
+++ inc/header.php
@@ -1,14 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="header navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="products.php">Products</a>
+        <a class="nav-link" href="products.php">Produits</a>
       </li>
       <li class="nav-item active">
         <a class="nav-link" href="contact.php">Contact</a>
@@ -21,7 +21,7 @@
             <li class="nav-item active">
             	<a class="nav-link" href="add-content.php">Modifier liste produits</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item deconnexion">
             	<a class="nav-link" href="index.php?deco">Déconnexion</a>
           	</li>
             <?php
@@ -35,12 +35,26 @@
               <a class="nav-link" href="add-content.php">Modifier liste produits</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="index.php?deco">Déconnexion</a>
+            <a class="nav-link deconnexion" href="index.php?deco">Déconnexion</a>
           	</li>
             <?php
             }
       }
       ?>
     </ul>
+    <h1 col-md-3">VINTAGE SHOP</h1>
+    <style>
+    
+    .header h1{
+		font-size: xx-large;
+		font-family: 'Pacifico', cursive;
+		margin-right: 45%;
+		}
+	.deconnexion{
+		margin-left:80%;
+	}
+
+
+	</style>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -7,6 +7,22 @@ require_once('inc/connexion.php');
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<style>
+		.background{
+			background-image: url("files/photo_couverture.jpg");
+			width:100%;
+			height: 100vh;
+		}
+		.description{
+			background: rgba(255,255,255,0.5);;
+
+		}
+		.description h1{
+			font-size: xx-large;
+		}
+
+	</style>
 </head>
 <body>
 	<?php require_once('inc/header.php');
@@ -18,7 +34,25 @@ require_once('inc/connexion.php');
             header('location:index.php');
     }
 
+
 	?>
+
+	<div class="row background">
+		<div class="col-md-3 description text-center">
+			<div class="row">
+				<h1 class="col-md-12 mt-3">A propos de nous</h1>
+				<p></p>
+			</div>
+
+			<div class="row mt-5">
+				<div class="col-md-12">
+					<p>Vintage Shop vous présente tous les articles que vous retrouverez dans nos magasins. Si vous aimez le style rock bla bla bla.....</p>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
 </body>
 </html>
 
