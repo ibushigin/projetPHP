80df0efab94a6a1d8a4732fcefad838e7ad7f9cc
Darpen <ben.peyrat@gmail.com>
2018-11-30 11:07:32 +0100

    V1

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="products.php">Products</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
