6d257b89b35ff58f1554ecc1ac4f4c1c897a237d
ibushigin <rq6v6k3>
2018-11-30 11:57:59 +0100

    add-user/ajoutuser

---
+++ add-user.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+require_once('inc/connexion.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ajout user</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+  <?php require_once('inc/header.php') ?>
+  <h2>Ajouter un nouvel utilisateur</h2>
+  <form method="post">
+    <label for="name">Entrez le nom </label>
+    <input type="text" name="name">
+    <label for="email">Entrez l'email</label>
+    <input type="text" name="email">
+    <label for="tel">Entrez le numéro de téléphone</label>
+    <input type="text" name="tel">
+    <label for="password">Entrez le mot de passe</label>
+    <input type="password" name="password">
+    <label for="password2">Répétez le mot de passe</label>
+    <input type="password" name="password2">
+    <label for="role">Choisissez le rôle</label>
+    <select name="role" id="role">
+      <option value="ROLE_USER">Admin</option>
+      <option value="ROLE_VENDOR">Vendeur</option>
+    </select>
+    <button type="submit" name="button">Envoyer</button>
+  </form>
+<!-- PHP POUR L'AJOUT DE L'USER -->
+<?php
+//Verif du POST
+if(!empty($_POST)){
+	$errors = [];
+	$post = [];
+	foreach($_POST as $key => $value){
+		$post[$key] = strip_tags($value);
+	}
+//Verif du nom
+	if(empty($post['name']) || mb_strlen($post['name']) < 4 || mb_strlen($post['name']) > 25){
+		$errors[] = 'Le nom doit faire entre 4 et 25 caractères';
+	}
+//Verif du mdp
+  if(!isset($post['password']) OR !isset($post['password2'])
+		OR mb_strlen($post['password']) < 4
+		OR mb_strlen($post['password']) > 10){
+      $errors['password'] = 'le mot de passe doit faire entre 4 et 10 caractères';
+  }
+  if($post['password'] !== $post['password2']){
+        $errors['mdp'] = 'Les deux mdp envoyés doivent être identiques';
+    }
+//Verif du role
+  if(!isset($post['role']) || (
+     ($post['role']) != 'ROLE_ADMIN'
+     &&($post['role']) != 'ROLE_VENDOR'){
+      $errors['role'] = 'rôle pas trouvé';
+    }
+//Verif de l'email
+	$select = $connexion->prepare('SELECT * FROM users WHERE email = :email');
+	$select->bindValue(':email', $post['email']);
+	$select->execute();
+	$users = $select->fetchAll();
+	if(count($users) > 0){
+		$errors[] = 'l\'email existe déjà';
+	}
+  if(empty($post['email']) OR !filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
+        $errors['email'] = 'email invalide';
+  }
+//Verif du telephone
+  if(empty($post['tel']) || !preg_match('#^0[1-9][0-9]{8}$#', $post['tel'])){
+      $errors[] = 'téléphone invalide';
+  }
+//Validé ===> insert
+  if(empty($errors)){
+  	$insert = $connexion->prepare('INSERT INTO users (name, email, password, tel, role) VALUES (:nom, :email, :password, :tel, :role)');
+  	$insert->bindValue(':name', $post['name']);
+  	$insert->bindValue(':email', $post['email']);
+  	$insert->bindValue(':password', password_hash($post['password'], PASSWORD_DEFAULT));
+    $insert->bindValue(':tel', $post['tel']);
+    $insert->bindValue(':role', $post['role']);
+  	if($insert->execute()){
+  		echo 'vous avez bien inscrit ' .$post['name'];
+  	}
+  	else{
+  		echo 'sql error';
+  	}
+  }
+  else{
+  	echo implode('<br>', $errors);
+  }
+}
+ ?>
+
+  <h2>Liste des utilisateurs</h2>
+
+
+</body>
+</html>
