96cd02ad20a8e64d2008e17854b394c2a11cf1e2
Darpen <ben.peyrat@gmail.com>
2018-11-30 13:33:33 +0100

    Update add-user.php

--- add-user.php
+++ add-user.php
@@ -11,7 +11,7 @@ require_once('inc/connexion.php');
 <body>
   <?php require_once('inc/header.php') ?>
   <div class="container">
-    <div class="row justify-content-center mb-5 mt-3">
+    <div class="row justify-content-around mb-5 mt-3">
       <div class="col-md-4">
       <form method="post">
         <h2>Ajouter un nouvel utilisateur</h2>
@@ -46,25 +46,23 @@ require_once('inc/connexion.php');
           <button class="btn btn-dark" type="submit" name="button">Envoyer</button>
         </form>
       </div>
-    </div>
-  </div>
 <!-- PHP POUR L'AJOUT DE L'USER -->
 <?php
 //Verif du POST
 if(!empty($_POST)){
-	$errors = [];
-	$post = [];
-	foreach($_POST as $key => $value){
-		$post[$key] = strip_tags($value);
-	}
+  $errors = [];
+  $post = [];
+  foreach($_POST as $key => $value){
+    $post[$key] = strip_tags($value);
+  }
 //Verif du nom
-	if(empty($post['name']) || mb_strlen($post['name']) < 4 || mb_strlen($post['name']) > 25){
-		$errors[] = 'Le nom doit faire entre 4 et 25 caractères';
-	}
+  if(empty($post['name']) || mb_strlen($post['name']) < 4 || mb_strlen($post['name']) > 25){
+    $errors[] = 'Le nom doit faire entre 4 et 25 caractères';
+  }
 //Verif du mdp
   if(!isset($post['password']) OR !isset($post['password2'])
-		OR mb_strlen($post['password']) < 4
-		OR mb_strlen($post['password']) > 10){
+    OR mb_strlen($post['password']) < 4
+    OR mb_strlen($post['password']) > 10){
       $errors['password'] = 'le mot de passe doit faire entre 4 et 10 caractères';
   }
   if($post['password'] !== $post['password2']){
@@ -75,59 +73,59 @@ if(!empty($_POST)){
       $errors['role'] = 'rôle pas trouvé';
     }
 //Verif de l'email
-	$select = $connexion->prepare('SELECT * FROM users WHERE email = :email');
-	$select->bindValue(':email', $post['email']);
-	$select->execute();
-	$users = $select->fetchAll();
-	if(count($users) > 0){
-		$errors[] = 'l\'email existe déjà';
-	}
+  $select = $connexion->prepare('SELECT * FROM users WHERE email = :email');
+  $select->bindValue(':email', $post['email']);
+  $select->execute();
+  $users = $select->fetchAll();
+  if(count($users) > 0){
+    $errors[] = 'l\'email existe déjà';
+  }
   if(empty($post['email']) OR !filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
         $errors['email'] = 'email invalide';
   }
 
 //Validé ===> insert
   if(empty($errors)){
-  	$insert = $connexion->prepare('INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)');
-  	$insert->bindValue(':name', $post['name']);
-  	$insert->bindValue(':email', $post['email']);
-  	$insert->bindValue(':password', password_hash($post['password'], PASSWORD_DEFAULT));
+    $insert = $connexion->prepare('INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)');
+    $insert->bindValue(':name', $post['name']);
+    $insert->bindValue(':email', $post['email']);
+    $insert->bindValue(':password', password_hash($post['password'], PASSWORD_DEFAULT));
     $insert->bindValue(':role', $post['role']);
-  	if($insert->execute()){
-  		echo 'vous avez bien inscrit ' .$post['name'];
-  	}
-  	else{
-  		echo 'sql error';
-  	}
+    if($insert->execute()){
+      echo 'vous avez bien inscrit ' .$post['name'];
+    }
+    else{
+      echo 'sql error';
+    }
   }
   else{
-  	echo implode('<br>', $errors);
+    echo implode('<br>', $errors);
   }
 }
  ?>
-
-  <h2>Modifier un utilisateur</h2>
-<?php
-$select = $connexion->query('SELECT id, name FROM users');
-$users = $select->fetchAll();
-?>
-<form>
-    <div class="form-group">
-        <label>Nom de l'utilisateur</label>
-        <select name="idUser" class="form-control">
-            <option value="0">Choisir un user</option>
-            <?php
-            foreach($users as $user){
-                ?>
-                <option value="<?= $user['id'] ?>"><?= $user['name'] ?></option>
-                <?php
-            }
-            ?>
-        </select>
-    </div>
-    <button class="btn btn-success">Choisir</button>
-</form>
-<?php
+    <div class="col-md-4">
+      <h2>Modifier un utilisateur</h2>
+      <?php
+      $select = $connexion->query('SELECT id, name FROM users');
+      $users = $select->fetchAll();
+      ?>
+        <form class="mb-5">
+            <div class="form-group">
+                <label>Nom de l'utilisateur</label>
+                <select name="idUser" class="form-control">
+                    <option value="0">Choisir un user</option>
+                    <?php
+                    foreach($users as $user){
+                        ?>
+                        <option value="<?= $user['id'] ?>"><?= $user['name'] ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </div>
+            <button class="btn btn-dark">Choisir</button>
+        </form>
+  <?php
 
 
 if(!empty($_GET['idUser']) && preg_match("#^\d+$#", $_GET['idUser'])){
@@ -137,26 +135,30 @@ if(!empty($_GET['idUser']) && preg_match("#^\d+$#", $_GET['idUser'])){
     $select->execute();
     $user = $select->fetch();
 ?>
- <form method="post">
-    <div class="form-group">
-        <label>Nom</label>
-        <input type="text" name="Nom" class="form-control" value="<?= $user['name'] ?>">
+<hr>
+        <form method="post" class="mt-5">
+          <div class="form-group">
+              <label>Nom</label>
+              <input type="text" name="Nom" class="form-control" value="<?= $user['name'] ?>">
+          </div>
+          <div class="form-group">
+              <label>Email</label>
+              <input type="text" name="email" class="form-control" value="<?= $user['email'] ?>">
+          </div>
+          <div class="form-group">
+              <label>Role</label>
+              <select name="role" class="form-control">
+                  <option value="0">Choisir un role</option>
+                  <option value="ROLE_AUTEUR" <?= $user['role'] === "ROLE_VENDOR" ? 'selected' : ''; ?> >Vendeur</option>
+                  <option value="ROLE_ADMIN" <?= $user['role'] === "ROLE_ADMIN" ? 'selected' : ''; ?> >admin</option>
+              </select>
+          </div>
+          <input type="hidden" name="idUser" value="<?= $_GET['idUser'] ?>">
+          <button type="submit" class="btn btn-dark">Ajouter</button>
+        </form>
+      </div>
     </div>
-    <div class="form-group">
-        <label>Email</label>
-        <input type="text" name="email" class="form-control" value="<?= $user['email'] ?>">
-    </div>
-    <div class="form-group">
-        <label>Role</label>
-        <select name="role" class="form-control">
-            <option value="0">Choisir un role</option>
-            <option value="ROLE_AUTEUR" <?= $user['role'] === "ROLE_VENDOR" ? 'selected' : ''; ?> >Vendeur</option>
-            <option value="ROLE_ADMIN" <?= $user['role'] === "ROLE_ADMIN" ? 'selected' : ''; ?> >admin</option>
-        </select>
-    </div>
-     <input type="hidden" name="idUser" value="<?= $_GET['idUser'] ?>">
-    <button type="submit" class="btn btn-info">Ajouter</button>
-</form>
+  </div>
 <?php
 }
 ?>
