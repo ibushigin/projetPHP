6bd68be7b6f9d2a600c264ecc1b54dc0899c67d5
ibushigin <rq6v6k3>
2018-12-03 15:21:42 +0100

    carte google op

--- contact.php
+++ contact.php
@@ -9,7 +9,34 @@ require_once('inc/connexion.php');
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
-  <?php require_once('inc/header.php') ?>
+  <?php
+	require_once('inc/header.php');
+	$select = $connexion -> query('SELECT address FROM address');
+	$address = $select -> fetch();
+	$map_address = $address['address'];
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?address={".urlencode($map_address)."}&key=AIzaSyBjslA2cbupRwG-dJvPAKcfZp0ruzEFM38";
 
+	$resultat = json_decode(file_get_contents($url, false), true);
+	//var_dump($resultat);
+	$lat = $resultat['results'][0]['geometry']['location']['lat'];
+	$lng = $resultat['results'][0]['geometry']['location']['lng'];
+	 ?>
+	 <div id="map"></div>
+	 <script>
+		 function initMap() {
+			 var shop = {lat: <?= $lat ?>,  lng: <?= $lng ?>};
+			 var map = new google.maps.Map(document.getElementById('map'), {
+				 zoom: 17,
+				 center: shop
+			 });
+			 var marker = new google.maps.Marker({
+				 position: shop,
+				 map: map
+			 });
+		 }
+	 </script>
+<script async defer
+					src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB0xJoi5c9MwYIYQlwIEfLqLh95hLtcaYA&callback=initMap">
+</script>
 </body>
 </html>
