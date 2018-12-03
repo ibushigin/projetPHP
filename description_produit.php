fecf62bce8284624962f837e965d2e4a7bb4fc74
Darpen <ben.peyrat@gmail.com>
2018-12-03 14:08:31 +0100

    new product

--- description_produit.php
+++ description_produit.php
@@ -23,7 +23,7 @@ require_once('inc/connexion.php');
 			<form class="col-md-2" method="GET" action="description_produit.php">
 				<!-- RECHERCHE PAR NOM -->
 				<div class="row mb-2">
-                	<input type="text" name="nom" placeholder="Nom" class="form-control col-md-12">
+                	<input type="text" name="name" placeholder="Nom" class="form-control col-md-12">
                 </div>
                 <!-- RECHERCHE PAR CATEGORIE -->
                 <div class="row mb-2">
@@ -51,22 +51,49 @@ require_once('inc/connexion.php');
             </form>
         </div>
 			
-			<!-- LISTE DES PRODUITS -->
+		<?php
+		if(!empty($_GET)){
 
-				<div class="row justify-content-around mt-5">
-			<?php
-				$select = $connexion->query('SELECT * FROM pictures INNER JOIN products ON pictures.id_product = products.id ORDER BY date_create DESC');
-				$products = $select->fetchAll();
+			$errors =[];
 
-				foreach($products as $product){
+			// RECHERCHE PAR NOM
+			if(isset($_GET['name']) && $_GET['name'] < 20){
+				$select = $connexion->prepare('SELECT * FROM pictures INNER JOIN products ON pictures.id_product = products.id WHERE name LIKE :name ORDER BY date_create DESC');
+        		$select->bindValue(':name', '%' . $_GET['name'] .'%');
+        		$select->execute();
+        		$products = $select->fetchAll();
+                foreach($products as $product){
+                    ?>
+					<article>
+						<h3><?= $product['name'] ?></h3>
+						<img src="files/thumbnails/<?= $product['file_name'] ?>">
+						<p>Prix : <?= $product['price'] ?>€</p>
+					</article>
+                    <?php
+                 }
+			}else{
+				$errors[] = 'Mauvaise saisie du nom';
+			}
+
+		}
+		else{			
 			?>
-				<article>
-					<h3><?= $product['name'] ?></h3>
-					<img src="files/thumbnails/<?= $product['file_name'] ?>">
-					<p>Prix : <?= $product['price'] ?>€</p>
-				</article>
-			<?php	
-				}
+				<!-- LISTE DES PRODUITS -->
+				<div class="row justify-content-around mt-5">
+				<?php
+					$select = $connexion->query('SELECT * FROM pictures INNER JOIN products ON pictures.id_product = products.id ORDER BY date_create DESC');
+					$products = $select->fetchAll();
+
+					foreach($products as $product){
+				?>
+					<article>
+						<h3><?= $product['name'] ?></h3>
+						<img src="files/thumbnails/<?= $product['file_name'] ?>">
+						<p>Prix : <?= $product['price'] ?>€</p>
+					</article>
+				<?php	
+					}
+		}
 			?>
 				</div>
 		</div>
