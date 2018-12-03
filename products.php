4756326f430e892689c1f641e26186955e32951c
Darpen <ben.peyrat@gmail.com>
2018-12-03 12:40:55 +0100

    Update products.php

--- products.php
+++ products.php
@@ -11,7 +11,41 @@ require_once('inc/connexion.php');
 	<div class="row ml-0">
 		<div class="col-md-12">
 			<h2 class="text-center mt-5 mb-5">Nos produits</h2>
-				<div class="row justify-content-around">
+
+			<!-- BARRE DE RECHERCHE -->
+			<?php
+
+			$select = $connexion->query('SELECT DISTINCT label FROM category ORDER BY label');
+            $categories = $select->fetchAll();
+
+            ?>
+		<div class="row justify-content-center">
+			<form class="col-md-2" method="GET" action="recherche_par_auteur.php">
+				<!-- RECHERCHE PAR NOM -->
+				<div class="row">
+                	<input type="text" name="nom" placeholder="Nom">
+                    <button type="submit" class="btn btn-primary btn-sm col-md-3">Rechercher</button>
+                </div>
+                <!-- RECHERCHE PAR CATEGORIE -->
+                <div class="row">
+                    <select name="category" class="form-control col-md-9">
+                        <option value="0">Choisir une catégorie</option>
+                        <?php
+                     
+                        foreach ($categories as $category) {
+                            ?>
+                            <option><?= $category['label'] ?></option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                    <button type="submit" class="btn btn-primary btn-sm col-md-3">Rechercher</button>
+                </div>
+            </form>
+        </div>
+			
+			<!-- LISTE DES PRODUITS -->
+				<div class="row justify-content-around mt-5">
 			<?php
 				$select = $connexion->query('SELECT * FROM pictures INNER JOIN products ON pictures.id_product = products.id ORDER BY date_create DESC');
 				$products = $select->fetchAll();
@@ -21,7 +55,7 @@ require_once('inc/connexion.php');
 				<article>
 					<h3><?= $product['name'] ?></h3>
 					<img src="files/thumbnails/<?= $product['file_name'] ?>">
-					<p><?= $product['price'] ?>€</p>
+					<p>Prix : <?= $product['price'] ?>€</p>
 				</article>
 			<?php	
 				}
