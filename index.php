346227a332d458b342c9dce141faa45af6ae4408
ibushigin <rq6v6k3>
2018-12-03 11:51:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,37 +2,64 @@
 session_start();
 require_once('inc/connexion.php');
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Home</title>
-
 	<?php require_once('inc/header.php');
-
 	if(isset($_GET['deco'])){
             //on supprime la session
             session_destroy();
             //on redirige vers la page d'accueil
             header('location:index.php');
     }
+    // BOUCLE SUR LE CONTENU
+    $resultat = $connexion->query('SELECT * FROM content ORDER BY id DESC LIMIT 1');
 
+    $contenu = $resultat->fetchAll();
 
+    foreach($contenu as $content){
 	?>
-
-	<div class="row background">
+	<div class="row background ml-0">
 		<div class="col-md-3 description text-center">
 			<div class="row">
-				<h2 class="col-md-12 mt-3">A propos de nous</h2>
-				<p></p>
+				<h2 class="col-md-12 mt-3"><?= $content['title'] ?></h2>
 			</div>
-
 			<div class="row mt-5">
 				<div class="col-md-12">
-					<p>Vintage Shop vous présente tous les articles que vous retrouverez dans nos magasins. Si vous aimez le style rock bla bla bla.....</p>
+					<p><?= $content['p1'] ?></p>
 				</div>
 			</div>
-
-
+			<div class="row mt-5">
+				<div class="col-md-12">
+					<p><?= $content['p2'] ?></p>
+				</div>
+			</div>
+		</div>
+	<?php
+    }
+    ?>
+		<div class="col-md-7">
+		</div>
+		<!-- BOUCLE SUR LES IMAGES -->
+		<div class="col-md-2 description text-center">
+			<div class="row">
+				<h2 class="col-md-12 mt-3">Derniers produits ajoutés</h2>s
+			</div>
+			<div class="row mt-5">
+				<div class="col-md-12 flex-column">
+				<?php
+					$select = $connexion->query('SELECT * FROM pictures ORDER BY id DESC LIMIT 4');
+					$products = $select->fetchAll();
+					foreach($products as $product){
+				?>
+					<img src="files/thumbnails/<?= $product['file_name'] ?>">
+				<?php
+					}
+				?>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
